5dbd1457133b4f1cc1a2ef60961bd099719c25aa
Jean-Philippe Boily <jipiboily@gmail.com>
2012-02-13 16:24:56 -0500

    un chantier...

--- app/assets/javascripts/admin/spree_multi_lingual.js
+++ app/assets/javascripts/admin/spree_multi_lingual.js
@@ -1,27 +1,45 @@
 //= require admin/spree_core
 
+
+
 $(document).ready(function(){
+  
   $("#spree_multi_lingual_dropdown").change(function(e){
-//    alert("ok");
-    $("input[name='translated_fields']").each(function(index, field){
-      alert($(field).val());
-    });
+    SpreeMultiLingual.change_language();
   });
 });
 
 
 var SpreeMultiLingual = {}
 (SpreeMultiLingual = function () {
+    SpreeMultiLingual.translated_fields = [];
     
     SpreeMultiLingual.change_language = function () {
-      
+      set_translated_fields();
+      for_each_field(function(field){
+        alert(field);
+      });
     }
     
     SpreeMultiLingual.duplicate_fields = function(){
       
     }
     
-    SpreeMultiLingual.fields_exists_for_language = function(){
+    SpreeMultiLingual.field_exists_for_language = function(){
       
+    }
+    
+    set_translated_fields = function(){
+      if(SpreeMultiLingual.translated_fields.length == 0){
+        $("input[name='translated_fields']").each(function(i, field){
+          SpreeMultiLingual.translated_fields[i] = $(field).val();
+        });
+      }
+    }
+    
+    for_each_field = function(callback){
+      for(var i = 0; i > SpreeMultiLingual.translated_fields.length; i++){
+        eval(callback + "(" + SpreeMultiLingual.translated_fields[i] + ")");
+      }
     }
 })();
