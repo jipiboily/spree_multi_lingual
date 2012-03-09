3149b09c45bafd7b8bb50df277f588234a01bda8
Bounmy Stephane <stephanebounmy@gmail.com>
2012-03-09 15:01:10 +0100

    refactored language_dropdown to be more flexible

--- app/overrides/product_add_language_dropdown.rb
+++ app/overrides/product_add_language_dropdown.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
         :virtual_path  => "spree/admin/products/_form",
         :insert_top => "[data-hook='admin_product_form_left']",
-        :partial => "spree/admin/spree_multi_lingual/products/language_dropdown",
+        :text => '<%= render "spree/admin/shared/language_dropdown", :object => @product -%>',
         :name    => "product_add_language_dropdown")
