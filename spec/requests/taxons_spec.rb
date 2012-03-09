acdeb7202889ac31b1667a2e6721ec1b94c379fd
Bounmy Stephane <stephanebounmy@gmail.com>
2012-03-09 17:21:38 +0100

    add taxons multi language test

--- spec/requests/taxons_spec.rb
+++ spec/requests/taxons_spec.rb
@@ -29,4 +29,34 @@ feature "Products multi lingual", :js => true do
     click_link "es"
     page.should have_content("Hola")
   end
+
+  scenario "admin should be able to edit taxons" do
+    visit spree.new_admin_taxonomy_path
+
+    fill_in "Name", :with => "Hello"
+    click_button "Create"
+
+    taxonomy = Spree::Taxonomy.last
+
+    visit spree.edit_admin_taxonomy_taxon_path(taxonomy, taxonomy.root)
+
+    %w(fr en es).each do |locale|
+      select locale, :from => "spree_multi_lingual_dropdown"
+
+      suffix = "#{locale == "en" ? "" : "_#{locale}"}"
+
+      fill_in "taxon_name#{suffix}", :with => "TAXON - #{locale}"
+      fill_in "taxon_description#{suffix}", :with => "TAXON Description - #{locale * 20}"
+    end
+
+    click_button "Update"
+
+    visit spree.edit_admin_taxonomy_taxon_path(taxonomy, taxonomy.root)
+    %w(fr en es).each do |locale|
+      select locale, :from => "spree_multi_lingual_dropdown"
+      page.should have_content("TAXON - #{locale}")
+      page.should have_content("TAXON Description - #{locale * 20}")
+    end
+
+  end
 end
