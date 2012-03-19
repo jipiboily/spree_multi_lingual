a7d3ad8a31e7c77c8983b88b8afaab961efa7067
Ryan Bigg <radarlistener@gmail.com>
2012-03-19 10:17:45 -0600

    Make product_add_language_dropdown override prettier

--- app/overrides/product_add_language_dropdown.rb
+++ app/overrides/product_add_language_dropdown.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(
-        :virtual_path  => "spree/admin/products/_form",
-        :insert_top => "[data-hook='admin_product_form_left']",
-        :text => '<%= render "spree/admin/shared/language_dropdown", :object => @product -%>',
-        :name    => "product_add_language_dropdown")
+  :virtual_path => "spree/admin/products/_form",
+  :insert_top   => "[data-hook='admin_product_form_left']",
+  :text         => '<%= render "spree/admin/shared/language_dropdown", :object => @product -%>',
+  :name         => "product_add_language_dropdown"
+)
