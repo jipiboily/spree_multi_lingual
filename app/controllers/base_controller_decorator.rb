4ff15ddd6b937e01404dee109b627e1775d6d2f6
Bounmy Stephane <stephanebounmy@gmail.com>
2012-04-10 21:41:23 +0200

    settting default locale for amdin

--- app/controllers/base_controller_decorator.rb
+++ app/controllers/base_controller_decorator.rb
@@ -26,4 +26,11 @@ Spree::Admin::BaseController.class_eval do
     locale ||= I18n.locale
     I18n.locale == locale.to_sym ? "".to_sym : "_"+ locale
   end
+
+  private
+
+  def set_user_language
+    I18n.locale = Spree::Config[:default_locale].to_sym
+  end
+
 end
