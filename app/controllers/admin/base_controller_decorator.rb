21040855affe557db8c6ab6b325ab0d9b7888879
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-05 15:43:03 +0200

    decorate set_user_language at higher level (module)

---
+++ app/controllers/admin/base_controller_decorator.rb
@@ -0,0 +1,16 @@
+Spree::Admin::BaseController.class_eval do
+  # removed link to /products
+  helper_method :locale_suffix
+
+  def locale_suffix(locale)
+    locale ||= I18n.locale
+    I18n.locale == locale.to_sym ? "".to_sym : "_"+ locale
+  end
+
+  private
+
+  def set_user_language
+    I18n.locale = Spree::Config[:default_locale].to_sym
+  end
+
+end
