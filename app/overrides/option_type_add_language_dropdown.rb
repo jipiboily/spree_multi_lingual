94c3b3e508f011e53d1c8ae2d079b9711b69459f
Brice Sanchez <brice.sanchez@nurun.com>
2012-10-18 14:42:59 -0400

    Deleting language dropdown in option type index

--- app/overrides/option_type_add_language_dropdown.rb
+++ app/overrides/option_type_add_language_dropdown.rb
@@ -1,11 +1,4 @@
 Deface::Override.new(
-  :virtual_path => "spree/admin/option_types/index",
-  :insert_before   => "div#new_option_type",
-  :text         => '<%= render "spree/admin/shared/language_dropdown", :object => Spree::OptionType.new -%>',
-  :name         => "option_type_add_language_dropdown"
-)
-
-Deface::Override.new(
   :virtual_path => "spree/admin/option_types/edit",
   :insert_before   => "fieldset",
   :text         => '<%= render "spree/admin/shared/language_dropdown", :object => @option_type -%>',
