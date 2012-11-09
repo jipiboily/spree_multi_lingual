06dd1636fb94ad948d714eb353306e4f971cd527
Filipe Dias <f.dias@karma-consulting.pt>
2012-11-09 18:11:35 +0000

    Updated added migrations to migrate existing data. Added rake task for the same purpose.

--- db/migrate/20121024155119_add_translation_to_tax_category.rb
+++ db/migrate/20121024155119_add_translation_to_tax_category.rb
@@ -1,6 +1,6 @@
 class AddTranslationToTaxCategory < ActiveRecord::Migration
   def up
-    Spree::TaxCategory.create_translation_table! :name => :string
+    Spree::TaxCategory.create_translation_table! ( { :name => :string }, { :migrate_data => true } )
   end
   def down
     Spree::TaxCategory.drop_translation_table!
