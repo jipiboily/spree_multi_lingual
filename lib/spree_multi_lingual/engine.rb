e8e33c910757bdf0abf3b6c25ce52d40305c967d
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-02-27 12:30:38 -0500

    Multilingual search. Ref #4
    - Based on @jipiboily solution, made it cleaner using chained method to avoid repeating spree core search logic & made cleaner SQL query building.

--- lib/spree_multi_lingual/engine.rb
+++ lib/spree_multi_lingual/engine.rb
@@ -18,7 +18,7 @@ module SpreeMultiLingual
     end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../../{app,lib}/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
