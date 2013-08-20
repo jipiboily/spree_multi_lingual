a9eef2bca72fa7391b079d57cce5ea34a831b5c7
Stephane Bounmy <stephanebounmy@gmail.com>
2013-08-20 12:56:49 +0200

    fix regression due to spree commit 20b5ba145bb777d38afdb25b78781c940c2b0002 id was passed to mailer instead of model object

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -2,11 +2,11 @@ Spree::ShipmentMailer.class_eval do
   helper 'spree/base'
 
   def shipped_email(shipment, resend=false)
-    @shipment = shipment
+    @shipment = shipment.is_a?(Spree::Shipment) ? shipment : Spree::Shipment.find(shipment)
     I18n.locale = @shipment.order.locale
     subject = (resend ? "[#{t(:resend).upcase}] " : "")
-    subject += "#{Spree::Config[:site_name]} #{t('shipment_mailer.shipped_email.subject')} ##{shipment.order.number}"
-    mail(:to => shipment.order.email,
+    subject += "#{Spree::Config[:site_name]} #{t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
+    mail(:to => @shipment.order.email,
     :subject => subject)
   end
 end
