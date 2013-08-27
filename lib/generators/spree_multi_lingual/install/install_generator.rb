75901984245413e6ff53a7de18e48e5c0eee5b6f
Stephane Bounmy <stephanebounmy@gmail.com>
2013-08-27 08:34:12 +0200

    fixed generator

--- lib/generators/spree_multi_lingual/install/install_generator.rb
+++ lib/generators/spree_multi_lingual/install/install_generator.rb
@@ -1,7 +1,7 @@
 module SpreeMultiLingual
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      class_option :yes, :type => :boolean, :default => false
+      class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require store/spree_multi_lingual\n"
@@ -18,12 +18,12 @@ module SpreeMultiLingual
       end
 
       def run_migrations
-         res = options[:yes] ?  "" : ask("Would you like to run the migrations now? [Y/n]")
-         if res == "" || res.downcase == "y"
-           run 'bundle exec rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
