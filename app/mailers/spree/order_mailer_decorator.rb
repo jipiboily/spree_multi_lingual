8e6da597de98837132dab4a96b53146556e1710f
Stephane Bounmy <stephanebounmy@gmail.com>
2013-10-10 00:32:08 +0200

    fix mailer

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -10,6 +10,7 @@ Spree::OrderMailer.class_eval do
 
   def cancel_email(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    I18n.locale = @order.locale
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
     mail(to: @order.email, from: from_address, subject: subject)
--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -4,8 +4,8 @@ Spree::ShipmentMailer.class_eval do
   def shipped_email(shipment, resend=false)
     @shipment = shipment.is_a?(Spree::Shipment) ? shipment : Spree::Shipment.find(shipment)
     I18n.locale = @shipment.order.locale
-    subject = (resend ? "[#{t(:resend).upcase}] " : "")
-    subject += "#{Spree::Config[:site_name]} #{t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : "")
+    subject += "#{Spree::Config[:site_name]} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
     mail(:to => @shipment.order.email,
     :subject => subject,
     :from => from_address)
